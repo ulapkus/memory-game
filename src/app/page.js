75a031c109bd3e0e9e6adeb6f0ef0bee57976a0e
ulapkus <ulapkus@gmail.com>
2023-09-19 15:38:08 -0500

    fixed bugs and final updates

--- src/app/page.js
+++ src/app/page.js
@@ -22,6 +22,7 @@ function Game() {
   const [count, setCount] = useState(0);
   const [timerIsRunning, setTimerIsRunning] = useState(false);
   const [time, setTime] = useState(60);
+  const [howfast, setHowfast] = useState(0);
 
   React.useEffect(() => {
     let interval;
@@ -29,19 +30,14 @@ function Game() {
       interval = setInterval(() => {
         setTime((prevTime) => prevTime - 1);
       }, 1000);
-    }
-    if (timerIsRunning === false) {
-      // setCount(0);
-      // setTime(60);
+    } else if (timerIsRunning === false) {
       clearInterval(interval);
     }
 
     if (timerIsRunning && time === 0) {
-      document.getElementById("myModal").style.display = "block";
+      document.getElementById("loseModal").style.display = "block";
       setTimerIsRunning(false);
       clearInterval(interval);
-      // setCount(0);
-      // setTime(60);
     }
 
     if (count === 12) {
@@ -51,7 +47,7 @@ function Game() {
     return () => {
       clearInterval(interval);
     };
-  }, [timerIsRunning, time, count]);
+  }, [timerIsRunning, time]);
 
   useEffect(() => {
     function onstartup() {
@@ -60,19 +56,15 @@ function Game() {
     onstartup();
   }, []);
 
-  function salir() {
+  function exit() {
     const openmodal = document.getElementById("openModal");
     openmodal.style.display = "none";
-  }
-
-  function exit() {
-    const mymodal = document.getElementById("myModal");
-    mymodal.style.display = "none";
+    setTimerIsRunning(true);
   }
 
   function tryagain() {
-    const mymodal = document.getElementById("myModal");
-    mymodal.style.display = "none";
+    const losemodal = document.getElementById("loseModal");
+    losemodal.style.display = "none";
     setCount(0);
     setTime(60);
     setTimerIsRunning(true);
@@ -87,31 +79,19 @@ function Game() {
   function leave() {
     const successmodal = document.getElementById("successModal");
     successmodal.style.display = "none";
+    setCount(0);
+    setTime(60);
+    setTimerIsRunning(true);
   }
 
-  window.onclick = function (event) {
-    const openmodal = document.getElementById("openModal");
-    const mymodal = document.getElementById("myModal");
-    const successmodal = document.getElementById("successModal");
-    if (
-      event.target == successmodal ||
-      event.target == mymodal ||
-      event.target == openmodal
-    ) {
-      successmodal.style.display = "none";
-      mymodal.style.display = "none";
-      openmodal.style.display = "none";
-    }
-  };
-
   const shuffleArray = (e) => {
-    setTimerIsRunning(true);
     if (e.target.tagName === "IMG") {
       setClickedArr((prevClickedArr) => [
         ...prevClickedArr,
         e.target.getAttribute("src"),
       ]);
     }
+
     const shuffled = [...gameArr];
     for (let i = gameArr.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -126,7 +106,7 @@ function Game() {
 
       setTimerIsRunning(false);
       setClickedArr([]);
-      document.getElementById("myModal").style.display = "block";
+      document.getElementById("loseModal").style.display = "block";
     }
   };
 
@@ -134,16 +114,20 @@ function Game() {
     if (count === 12) {
       document.getElementById("successModal").style.display = "block";
       setTimerIsRunning(false);
+      const secondsleft = time;
+      const result = 60 - secondsleft;
+      setHowfast(result);
     }
   }, [timerIsRunning, time, count]);
 
+  const begintimer = () => {
+    setTimerIsRunning(true);
+  };
+
   return (
-    <div className={styles.allcontent}>
-      <div id="openModal" className={styles.modal}>
-        <span onClick={salir} className={styles.close}>
-          &times;
-        </span>
-        <div className={styles.modalcontent}>
+    <section className={styles.all_content}>
+      <section id="openModal" className={styles.modal}>
+        <div className={styles.intro_modal}>
           <div className={styles.headings}>
             <h3>Oh no!!</h3>
             <h4>
@@ -151,63 +135,64 @@ function Game() {
               Forest.
             </h4>
           </div>
-          <p className={styles.openModalWords}>
+          <p className={styles.intro_modal_words}>
             Save them by clicking each one ONCE before someone finds out they
             are here.
           </p>
+          <button onClick={exit}>Begin</button>
         </div>
+      </section>
+      <div className={styles.heading_and_newgame}>
+        <h1>Neopets Memory Game</h1>
+        <p className={styles.newgame} onClick={newgame}>
+          New Game
+        </p>
       </div>
-      <div className={styles.headingPlusNewGame}>
-      <h1>Neopets Memory Game</h1>
-      <p className={styles.newgame} onClick={newgame}>New Game</p>
-      </div>
-      <div className={styles.timeandcount}>
+      <div className={styles.time_and_count}>
         <h2>Time left: {time} seconds</h2>
-        <p className={styles.currentCount}>Neopets rescued: {count}</p>
+        <p className={styles.current_count}>Neopets rescued: {count}</p>
       </div>
 
-      <section className={styles.images}>
-        <img onClick={shuffleArray} className={styles.pet} src={gameArr[0]} />
-        <img onClick={shuffleArray} className={styles.pet} src={gameArr[1]} />
-        <img onClick={shuffleArray} className={styles.pet} src={gameArr[2]} />
-        <img onClick={shuffleArray} className={styles.pet} src={gameArr[3]} />
-        <img onClick={shuffleArray} className={styles.pet} src={gameArr[4]} />
-        <img onClick={shuffleArray} className={styles.pet} src={gameArr[5]} />
-        <img onClick={shuffleArray} className={styles.pet} src={gameArr[6]} />
-        <img onClick={shuffleArray} className={styles.pet} src={gameArr[7]} />
-        <img onClick={shuffleArray} className={styles.pet} src={gameArr[8]} />
-        <img onClick={shuffleArray} className={styles.pet} src={gameArr[9]} />
-        <img onClick={shuffleArray} className={styles.pet} src={gameArr[10]} />
-        <img onClick={shuffleArray} className={styles.pet} src={gameArr[11]} />
+      <section className={styles.images} onClick={shuffleArray}>
+        <img className={styles.neopet_img} src={gameArr[0]} />
+        <img className={styles.neopet_img} src={gameArr[1]} />
+        <img className={styles.neopet_img} src={gameArr[2]} />
+        <img className={styles.neopet_img} src={gameArr[3]} />
+        <img className={styles.neopet_img} src={gameArr[4]} />
+        <img className={styles.neopet_img} src={gameArr[5]} />
+        <img className={styles.neopet_img} src={gameArr[6]} />
+        <img className={styles.neopet_img} src={gameArr[7]} />
+        <img className={styles.neopet_img} src={gameArr[8]} />
+        <img className={styles.neopet_img} src={gameArr[9]} />
+        <img className={styles.neopet_img} src={gameArr[10]} />
+        <img className={styles.neopet_img} src={gameArr[11]} />
       </section>
 
-      <div id="myModal" className={styles.modal}>
-        <span onClick={exit} className={styles.close}>
-          &times;
-        </span>
-        <div className={styles.modalcontentlose}>
+      <section id="loseModal" className={styles.modal} onClick={begintimer}>
+        <div className={styles.modal_lose}>
           <img
-            className={styles.evilThade}
+            className={styles.evil_thade}
             src="https://i.ibb.co/YhNbr74/Screenshot-2023-09-14-at-4-15-42-PM-removebg-preview-1.png"
           />
           <h5>Evil Thade found them!</h5>
           <p>You only saved {count} Neopet(s).</p>
           <button onClick={tryagain}>Try Again</button>
         </div>
-      </div>
-      <div id="successModal" className={styles.modal}>
-        <span onClick={leave} className={styles.close}>
-          &times;
-        </span>
-        <div className={styles.modalcontentwin}>
+      </section>
+      <section id="successModal" className={styles.modal}>
+        <div className={styles.modal_win}>
           <h6>You saved them all! </h6>
-
-          <p className={styles.successwords}>
-            Phew. Now they get to go home and rest.
-          </p>
+          <div className={styles.success_words}>
+            <p>And it only took you {howfast} seconds.</p>
+            <p>Phew. Now they get to go home and rest.</p>
+          </div>
+          <div>
+            <p className={styles.faster}>Think you can do faster?</p>
+            <button onClick={leave}>Try Again</button>
+          </div>
         </div>
-      </div>
-    </div>
+      </section>
+    </section>
   );
 }
 
