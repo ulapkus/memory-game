b4b5f49801790efbfdb2d7c1338365aa957d71b5
ulapkus <ulapkus@gmail.com>
2024-04-02 22:44:33 -0400

    added difficulty levels

--- src/app/page.js
+++ src/app/page.js
@@ -5,24 +5,31 @@ import styles from "./page.module.css";
 
 function Game() {
   const [gameArr, setGameArr] = useState([
-    "https://pets.neopets.com/cp/6mn6k8lv/1/5.png",
-    "https://pets.neopets.com/cp/2wsqgt78/1/7.png",
-    "https://pets.neopets.com/cp/nwx8v2rb/1/4.png",
-    "https://pets.neopets.com/cp/sk22m495/1/7.png",
-    "https://pets.neopets.com/cp/qn8kjbwf/1/4.png",
-    "https://pets.neopets.com/cp/rfsbh59t/1/5.png",
-    "https://pets.neopets.com/cp/g48kdbqq/1/4.png",
-    "https://pets.neopets.com/cp/v79jtf2c/1/4.png",
-    "https://pets.neopets.com/cp/9dq8hcr2/1/4.png",
-    "https://pets.neopets.com/cp/vrcgjm3q/1/4.png",
-    "https://pets.neopets.com/cp/c39wz4r9/1/7.png",
-    "https://pets.neopets.com/cp/kfonqhdc/1/7.png",
+    // "https://imagetolink.com/ib/AZnQhJmJZ7.png",
+    // "https://imagetolink.com/ib/4pJgH2eYfJ.png",
+
+    // "https://imagetolink.com/ib/f7kXVMTxqa.png",
+    // "https://imagetolink.com/ib/Em5Vse71iC.png",
+    // "https://imagetolink.com/ib/Xy0K1LDu7L.png",
+    "https://imagetolink.com/ib/hG4ledtobm.png",
+    "https://imagetolink.com/ib/T4A0n1Jc9k.png",
+    "https://imagetolink.com/ib/5hdb0TKwOt.png",
+    "https://imagetolink.com/ib/sABYVQFCaF.png",
+    "https://imagetolink.com/ib/rCihSQ2lbB.png",
+    "https://imagetolink.com/ib/sY4DQFCWfs.png",
+    "https://imagetolink.com/ib/I0idzCTEvf.png",
+    "https://imagetolink.com/ib/X8b1tora8J.png",
+    "https://imagetolink.com/ib/CUVx2f719J.png",
+    "https://imagetolink.com/ib/U6k4FYP0Xf.png",
+    "https://imagetolink.com/ib/FDxgvwhXXz.png",
+    "https://imagetolink.com/ib/JDhaQ9uYu2.png",
   ]);
   const [clickedArr, setClickedArr] = useState([]);
   const [count, setCount] = useState(0);
   const [timerIsRunning, setTimerIsRunning] = useState(false);
   const [time, setTime] = useState(60);
   const [howfast, setHowfast] = useState(0);
+  const [gameMode, setGameMode] = useState("medium");
 
   React.useEffect(() => {
     let interval;
@@ -62,6 +69,13 @@ function Game() {
     setTimerIsRunning(true);
   }
 
+  const submitMode = (mode) => {
+    setGameMode(mode);
+    const modemodal = document.getElementById("modeModal");
+    modemodal.style.display = "none";
+    setTimerIsRunning(true);
+  };
+
   function tryagain() {
     const losemodal = document.getElementById("loseModal");
     losemodal.style.display = "none";
@@ -74,6 +88,13 @@ function Game() {
     setCount(0);
     setTime(60);
     setTimerIsRunning(true);
+  }
+
+  function chooseMode() {
+    document.getElementById("modeModal").style.display = "block";
+    setCount(0);
+    setTime(60);
+    setTimerIsRunning(false);
   }
 
   function leave() {
@@ -124,86 +145,299 @@ function Game() {
     setTimerIsRunning(true);
   };
 
-  return (
-    <div>
-      <section className={styles.all_content}>
-        <section id="openModal" className={styles.modal}>
-          <div className={styles.intro_modal}>
-            <div className={styles.headings}>
-              <h3>Oops!</h3>
-              <h4>
-                The Neopets wandered too far and became lost in the Haunted
-                Forest.
-              </h4>
+  const renderEasyMode = () => {
+    return (
+      <div>
+        <section className={styles.all_content}>
+          <section id="openModal" className={styles.modal}>
+            <div className={styles.intro_modal}>
+              <div className={styles.headings}>
+                <h3>Oops!</h3>
+                <h4>All the art got jumbled up.</h4>
+              </div>
+              <p className={styles.intro_modal_words}>
+                Re-organize the paintings by clicking each one ONCE before
+                someone finds out what happened.
+              </p>
+              <button onClick={exit}>Begin</button>
             </div>
-            <p className={styles.intro_modal_words}>
-              Save them by clicking each one ONCE before someone finds out they
-              are here.
-            </p>
-            <button onClick={exit}>Begin</button>
-          </div>
-        </section>
-        <div className={styles.heading_and_newgame}>
-          <h1>Neopets Memory Game</h1>
-          <p className={styles.newgame} onClick={newgame}>
-            New Game
-          </p>
-        </div>
-        <div className={styles.time_and_count}>
-          <h2>Time left: {time} seconds</h2>
-          <p className={styles.current_count}>Neopets rescued: {count}</p>
-        </div>
-
-        <section className={styles.images} onClick={shuffleArray}>
-          <img className={styles.neopet_img} src={gameArr[0]} />
-          <img className={styles.neopet_img} src={gameArr[1]} />
-          <img className={styles.neopet_img} src={gameArr[2]} />
-          <img className={styles.neopet_img} src={gameArr[3]} />
-          <img className={styles.neopet_img} src={gameArr[4]} />
-          <img className={styles.neopet_img} src={gameArr[5]} />
-          <img className={styles.neopet_img} src={gameArr[6]} />
-          <img className={styles.neopet_img} src={gameArr[7]} />
-          <img className={styles.neopet_img} src={gameArr[8]} />
-          <img className={styles.neopet_img} src={gameArr[9]} />
-          <img className={styles.neopet_img} src={gameArr[10]} />
-          <img className={styles.neopet_img} src={gameArr[11]} />
-        </section>
-
-        <section id="loseModal" className={styles.modal} onClick={begintimer}>
-          <div className={styles.modal_lose}>
-            <div className={styles.lose_all}>
-              <img
-                className={styles.evil_thade}
-                src="https://i.ibb.co/YhNbr74/Screenshot-2023-09-14-at-4-15-42-PM-removebg-preview-1.png"
-              />
-              <div className={styles.modal_lose_all_words}>
-                <h5>Evil Thade found them!</h5>
-                <p className={styles.modal_lose_words}>
-                  You only saved {count} Neopet(s).
-                </p>
-                <button onClick={tryagain}>Try Again</button>
+          </section>
+          <section id="modeModal" className={styles.modal}>
+            <div className={styles.intro_modal}>
+              <div className={styles.headings}>
+                <h3>Choose your difficulty level:</h3>
+                <button onClick={() => submitMode("easy")}>Easy</button>
+                <button onClick={() => submitMode("medium")}>Medium</button>
+                <button onClick={() => submitMode("hard")}>Hard</button>
               </div>
             </div>
+          </section>
+          <div className={styles.heading_and_newgame}>
+            <h1>Museum Memory Game</h1>
+            <p className={styles.newgame} onClick={newgame}>
+              New Game
+            </p>
+            <p className={styles.newgame} onClick={chooseMode}>
+              Game Mode
+            </p>
           </div>
-        </section>
-        <section id="successModal" className={styles.modal}>
-          <div className={styles.modal_win}>
-            <h6>You saved them all! </h6>
-            <div className={styles.success_words}>
-              <p className={styles.success_words_individual}>
-                And it only took you {howfast} seconds.
-              </p>
-              <p className={styles.success_words_individual}>
-                Phew. Now they get to go home and rest.
-              </p>
+          <div className={styles.time_and_count}>
+            <h2>Time left: {time} seconds</h2>
+            <p className={styles.current_count}>Art collected: {count}</p>
+          </div>
+
+          <section className={styles.images_easy} onClick={shuffleArray}>
+            <img className={styles.neopet_img_easy} src={gameArr[0]} />
+            <img className={styles.neopet_img_easy} src={gameArr[1]} />
+            <img className={styles.neopet_img_easy} src={gameArr[2]} />
+            <img className={styles.neopet_img_easy} src={gameArr[3]} />
+            <img className={styles.neopet_img_easy} src={gameArr[4]} />
+            <img className={styles.neopet_img_easy} src={gameArr[5]} />
+            {/* <img className={styles.neopet_img} src={gameArr[6]} /> */}
+            {/* <img className={styles.neopet_img} src={gameArr[7]} />
+            <img className={styles.neopet_img} src={gameArr[8]} />
+            <img className={styles.neopet_img} src={gameArr[9]} />
+            <img className={styles.neopet_img} src={gameArr[10]} />
+            <img className={styles.neopet_img} src={gameArr[11]} /> */}
+          </section>
+
+          <section id="loseModal" className={styles.modal} onClick={begintimer}>
+            <div className={styles.modal_lose}>
+              <div className={styles.lose_all}>
+                <div className={styles.modal_lose_all_words}>
+                  <h5>Evil Thade found them!</h5>
+                  <p className={styles.modal_lose_words}>
+                    You only saved {count} Neopet(s).
+                  </p>
+                  <button onClick={tryagain}>Try Again</button>
+                </div>
+              </div>
             </div>
+          </section>
+          <section id="successModal" className={styles.modal}>
+            <div className={styles.modal_win}>
+              <h6>You saved them all! </h6>
+              <div className={styles.success_words}>
+                <p className={styles.success_words_individual}>
+                  And it only took you {howfast} seconds.
+                </p>
+                <p className={styles.success_words_individual}>
+                  Phew. Now they get to go home and rest.
+                </p>
+              </div>
+              <div>
+                <p className={styles.faster}>Think you can do faster?</p>
+                <button onClick={leave}>Try Again</button>
+              </div>
+            </div>
+          </section>
+        </section>
+      </div>
+    );
+  };
+
+  const renderMediumMode = () => {
+    return (
+      <div>
+        <section className={styles.all_content}>
+          <section id="openModal" className={styles.modal}>
+            <div className={styles.intro_modal}>
+              <div className={styles.headings}>
+                <h3>Oops!</h3>
+                <h4>
+                  It's your first day working at the museum and you fell asleep!
+                </h4>
+                <p className={styles.intro_modal_words_two}>
+                  On top of that, you just discovered someone jumbled up all the
+                  art.
+                </p>
+              </div>
+              <p className={styles.intro_modal_words}>
+                Make sure all the paintings are accounted for by clicking each
+                one ONCE before your boss finds out what happened.
+              </p>
+              <button className={styles.button_intro} onClick={exit}>
+                Begin
+              </button>
+            </div>
+          </section>
+          <section id="modeModal" className={styles.modal}>
+            <div className={styles.intro_modal}>
+              <div className={styles.headings}>
+                <h3>Choose your difficulty level:</h3>
+                <button onClick={() => submitMode("easy")}>Easy</button>
+                <button onClick={() => submitMode("medium")}>Medium</button>
+                <button onClick={() => submitMode("hard")}>Hard</button>
+              </div>
+            </div>
+          </section>
+          <div className={styles.heading_and_newgame}>
+            <h1>Museum Memory Game</h1>
             <div>
-              <p className={styles.faster}>Think you can do faster?</p>
-              <button onClick={leave}>Try Again</button>
+              <p className={styles.newgame} onClick={newgame}>
+                New Game
+              </p>
+              <p className={styles.newgame} onClick={chooseMode}>
+                Game Mode
+              </p>
             </div>
           </div>
+          <div className={styles.time_and_count}>
+            <p className={styles.time_medium}>Time left: {time} seconds</p>
+            <p className={styles.current_count_medium}>
+              Art collected: {count}
+            </p>
+          </div>
+
+          <section className={styles.images} onClick={shuffleArray}>
+            <img className={styles.neopet_img_medium} src={gameArr[0]} />
+            <img className={styles.neopet_img_medium} src={gameArr[1]} />
+            <img className={styles.neopet_img_medium} src={gameArr[2]} />
+            <img className={styles.neopet_img_medium} src={gameArr[3]} />
+            <img className={styles.neopet_img_medium} src={gameArr[4]} />
+            <img className={styles.neopet_img_medium} src={gameArr[5]} />
+            <img className={styles.neopet_img_medium} src={gameArr[6]} />
+            <img className={styles.neopet_img_medium} src={gameArr[7]} />
+            {/* <img className={styles.neopet_img} src={gameArr[8]} />
+            <img className={styles.neopet_img} src={gameArr[9]} /> */}
+            {/* <img className={styles.neopet_img} src={gameArr[10]} />
+            <img className={styles.neopet_img} src={gameArr[11]} /> */}
+          </section>
+
+          <section id="loseModal" className={styles.modal} onClick={begintimer}>
+            <div className={styles.modal_lose}>
+              <h5>You got caught!</h5>
+              <p className={styles.modal_lose_words}>
+                You only accounted for {count} art piece(s).
+              </p>
+              <button onClick={tryagain}>Try Again</button>
+            </div>
+          </section>
+          <section id="successModal" className={styles.modal}>
+            <div className={styles.modal_win}>
+              <h6>You did it!</h6>
+              <div className={styles.success_words}>
+                <p className={styles.success_words_individual}>
+                  And it only took you {howfast} seconds.
+                </p>
+                <p className={styles.success_words_individual}>
+                  Phew. That was close.
+                </p>
+              </div>
+              <div>
+                <p className={styles.faster}>Think you can do faster?</p>
+                <button onClick={leave}>Try Again</button>
+              </div>
+            </div>
+          </section>
         </section>
-      </section>
+      </div>
+    );
+  };
+
+  const renderHardMode = () => {
+    return (
+      <div>
+        <section className={styles.all_content}>
+          <section id="openModal" className={styles.modal}>
+            <div className={styles.intro_modal}>
+              <div className={styles.headings}>
+                <h3>Oops!</h3>
+                <h4>All the art got jumbled up.</h4>
+              </div>
+              <p className={styles.intro_modal_words}>
+                Re-organize the paintings by clicking each one ONCE before
+                someone finds out what happened.
+              </p>
+              <button onClick={exit}>Begin</button>
+            </div>
+          </section>
+          <section id="modeModal" className={styles.modal}>
+            <div className={styles.intro_modal}>
+              <div className={styles.headings}>
+                <h3>Choose your difficulty level:</h3>
+                <button onClick={() => submitMode("easy")}>Easy</button>
+                <button onClick={() => submitMode("medium")}>Medium</button>
+                <button onClick={() => submitMode("hard")}>Hard</button>
+              </div>
+            </div>
+          </section>
+          <div className={styles.heading_and_newgame}>
+            <h1>Museum Memory Game</h1>
+            <div>
+              <p className={styles.newgame} onClick={newgame}>
+                New Game
+              </p>
+              <p className={styles.newgame} onClick={chooseMode}>
+                Game Mode
+              </p>
+            </div>
+          </div>
+          <div className={styles.time_and_count}>
+            <h2>Time left: {time} seconds</h2>
+            <p className={styles.current_count}>Art collected: {count}</p>
+          </div>
+
+          <section className={styles.images} onClick={shuffleArray}>
+            <img className={styles.neopet_img} src={gameArr[0]} />
+            <img className={styles.neopet_img} src={gameArr[1]} />
+            <img className={styles.neopet_img} src={gameArr[2]} />
+            <img className={styles.neopet_img} src={gameArr[3]} />
+            <img className={styles.neopet_img} src={gameArr[4]} />
+            <img className={styles.neopet_img} src={gameArr[5]} />
+            <img className={styles.neopet_img} src={gameArr[6]} />
+            <img className={styles.neopet_img} src={gameArr[7]} />
+            <img className={styles.neopet_img} src={gameArr[8]} />
+            <img className={styles.neopet_img} src={gameArr[9]} />
+            <img className={styles.neopet_img} src={gameArr[10]} />
+            <img className={styles.neopet_img} src={gameArr[11]} />
+          </section>
+
+          <section id="loseModal" className={styles.modal} onClick={begintimer}>
+            <div className={styles.modal_lose}>
+              <div className={styles.lose_all}>
+                <img
+                  className={styles.evil_thade}
+                  src="https://i.ibb.co/YhNbr74/Screenshot-2023-09-14-at-4-15-42-PM-removebg-preview-1.png"
+                />
+                <div className={styles.modal_lose_all_words}>
+                  <h5>Evil Thade found them!</h5>
+                  <p className={styles.modal_lose_words}>
+                    You only saved {count} Neopet(s).
+                  </p>
+                  <button onClick={tryagain}>Try Again</button>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section id="successModal" className={styles.modal}>
+            <div className={styles.modal_win}>
+              <h6>You saved them all! </h6>
+              <div className={styles.success_words}>
+                <p className={styles.success_words_individual}>
+                  And it only took you {howfast} seconds.
+                </p>
+                <p className={styles.success_words_individual}>
+                  Phew. Now they get to go home and rest.
+                </p>
+              </div>
+              <div>
+                <p className={styles.faster}>Think you can do faster?</p>
+                <button onClick={leave}>Try Again</button>
+              </div>
+            </div>
+          </section>
+        </section>
+      </div>
+    );
+  };
+  return (
+    <div>
+      {gameMode === "easy"
+        ? renderEasyMode()
+        : gameMode === "medium"
+        ? renderMediumMode()
+        : renderHardMode()}
     </div>
   );
 }
